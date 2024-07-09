e6703de6dbef251da98da35aa55493ba311382d0
arushar99 <arushaherath97@outlook.com>
2024-07-09 09:01:05 -0500

    override all xc

---
+++ Models/DirectoryResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace FileBrowser.Models
+{
+    public class DirectoryResponse
+    {
+       
+        
+            public List<FileFolder> Files { get; set; }
+            public List<FileFolder> Folders { get; set; }
+            public string Path { get; set; }
+            public int FileCount { get; set; }
+            public int FolderCount { get; set; }
+            public long TotalSize { get; set; }
+        
+    }
+}
---
+++ Models/FileFolder.cs
@@ -0,0 +1,12 @@
+﻿namespace FileBrowser.Models
+{
+    public class FileFolder
+    {
+
+        public string? Name { get; set; }
+        public long Size { get; set; }
+        public bool IsFolder { get; set; }
+
+        public int? fileCount { get; set; }
+    }
+}
